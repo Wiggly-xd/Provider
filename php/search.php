c4adf4e78939306d9ccea09f49671c80ef691ad6
Niklas Jörgensen <niklas.jorgensen@learnet.se>
2020-10-23 14:29:23 +0200

    konton,privileges,bilder,söka

---
+++ php/search.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE HTML>
+<html>
+
+<center><h2>Sök efter blogg/wiki</h2></center>
+<form action = "display.php" method="get">
+<br>
+<center>
+<input type="text" name="search" size="48">
+<br></br>
+<input type="submit" value="submit">
+<input type="reset" value="Reset">
+</center>
+<br>
+</form>
+</body>
+</html>
