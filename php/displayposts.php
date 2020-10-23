22a378a9e61de2e1d42c5c30de2ebf757b089754
Wiggly <w.n.richardson2001@gmail.com>
2020-10-23 14:22:56 +0200

    preparing for merge

--- php/displayposts.php
+++ php/displayposts.php
@@ -14,6 +14,12 @@ $stmt = 'SELECT * FROM service, spage, post WHERE service.serviceID = post.servi
 $res = mysqli_query($mysqli, $stmt);
 
 while($qryresult = mysqli_fetch_array($res)){
-    echo '<div style="border: 1px solid black; width: 50vw; margin-left:auto; margin-right: auto; padding-left: 1vw;"><h1>' . $qryresult["serviceTitle"] . '</h1><br>' . '<p>Creation date: ' . $qryresult["serviceDate"] . '</p><p>wiki=1, blog=0: </p>' . $qryresult["serviceType"] . '<h2>' . $qryresult["postTitle"] . '</h2><br>' . '<p>content: ' . $qryresult["pText"] . '</p><br>' . '<p>Post date: ' . $qryresult["postDate"] . '</p></div>','<br/>';
+    echo '<div style="border: 1px solid black; width: 50vw; margin-left:auto; margin-right: auto; padding-left: 1vw;"><h1>' . 
+    $qryresult["serviceTitle"] . '</h1><br>' . '<p>Creation date: ' . 
+    $qryresult["serviceDate"] . '</p><p>wiki=1, blog=0: </p>' . 
+    $qryresult["serviceType"] . '<h2>' . 
+    $qryresult["postTitle"] . '</h2><br>' . '<p>content: ' . 
+    $qryresult["pText"] . '</p><br>' . '<p>Post date: ' . 
+    $qryresult["postDate"] . '</p></div>','<br/>';
 }
 ?>
