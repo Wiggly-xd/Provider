061fca048a765e9d96b6028b62c9701e0f8989db
Wiggly <w.n.richardson2001@gmail.com>
2020-10-28 14:49:56 +0100

    Tidied up structure

--- php/version.php
+++ php/version.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 $stmt2 = $mysqli->prepare("INSERT INTO history (historyDate, historyText, serviceID, historyImage) VALUES (?, ?, ?, ?)");
 
 $stmt2->bind_param("ssis", $date, $pText, $serviceID, $filepath);
