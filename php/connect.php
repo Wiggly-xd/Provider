2e04a356c488c8108802e67dd42f757c1da2f85b
Niklas Jörgensen <niklas.jorgensen@learnet.se>
2020-10-29 10:19:39 +0100

    -||-

--- php/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli('localhost', 'root',"", 'provider');
-
-$mysqli->set_charset("utf8");
-?>
--- php/test.js
+++
@@ -1,5 +0,0 @@
-$("input:checkbox").attr("checked", false).click(function()
-{
-    var shcolumn="."+$(this).attr("name");
-    $(shcolumn).toggle();
-})
