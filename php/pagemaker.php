f59fe216401003e795c6960bc32293ebcbcd6776
Niklas Jörgensen <niklas.jorgensen@learnet.se>
2020-10-29 10:19:23 +0100

    removing old code, preparing for merge

---
+++ php/pagemaker.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+        echo "<p>logged in as: " . $_SESSION['username'] . "!";
+    } else {
+        header('Location: login.html');
+    }
+    ?>
+    <form action="pagecheck.php" method="post" class="form">
+        <p>title</p>
+        <input type="text" name="serviceTitle" placeholder="title" required="required">
+        <p>service id</p>
+        <input type="text" name="serviceID" placeholder="service ID" required="required">
+        <p>Wiki</p>
+        <input type="radio" name="serviceType" value="1">
+        <p>Blog</p>
+        <input type="radio" name="serviceType" value="0">
+ 
+        <input type="submit" value="Submit" name="btn">
+    </form>
+</body>
+</html>
